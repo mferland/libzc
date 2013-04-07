b11f1ac7846be4db8f81eb2e0083a08d86432edf
Marc Ferland <marc.ferland@gmail.com>
2013-04-07 13:54:24 -0400

    yazc: add place holder for yazc cmd line tool

---
+++ yazc/main.c
@@ -0,0 +1,28 @@
+/*
+ *  zc - zip crack library
+ *  Copyright (C) 2013  Marc Ferland
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "libzc.h"
+#include "config.h"
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+   printf("yazc "PACKAGE_VERSION"\n");
+   return 0;
+}
