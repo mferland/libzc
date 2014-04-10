0da825e0393c2da023e3df59084827664fcbca86
Marc Ferland <marc.ferland@gmail.com>
2014-04-10 11:08:55 -0400

    libzc: code cleanup

--- lib/key2_reduce.c
+++ lib/key2_reduce.c
@@ -144,10 +144,10 @@ static void generate_all_key2i_with_bits_1_0(struct key_table *key2i_table, unsi
 }
 
 void key2r_compute_single(unsigned int key2i_plus_1,
-                        struct key_table *key2i,
-                        const unsigned short *key2i_bits_15_2,
-                        const unsigned short *key2im1_bits_15_2,
-                        unsigned int common_bits_mask)
+                          struct key_table *key2i,
+                          const unsigned short *key2i_bits_15_2,
+                          const unsigned short *key2im1_bits_15_2,
+                          unsigned int common_bits_mask)
 {
    const unsigned int key2i_bits31_8 = (key2i_plus_1 << 8) ^ crc_32_invtab[key2i_plus_1 >> 24];
    const unsigned int key2i_bits15_10_rhs = key2i_bits31_8 & common_bits_mask;
