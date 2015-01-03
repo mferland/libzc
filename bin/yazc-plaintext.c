8e6273a7545a68d4beb2ce179d0670eedf6c9195
Marc Ferland <marc.ferland@gmail.com>
2015-01-02 21:38:36 -0500

    ptext: code to parse file names and offsets

--- bin/yazc-plaintext.c
+++ bin/yazc-plaintext.c
@@ -20,33 +20,96 @@
 #include <getopt.h>
 #include <libgen.h>
 #include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
 
 #include "yazc.h"
 #include "libzc.h"
-#include "crc32.h"
 
 static const char short_opts[] = "h";
-static const struct option long_opts[] = 
+static const struct option long_opts[] =
 {
    {"help", no_argument, 0, 'h'},
    {NULL, 0, 0, 0}
 };
 
-struct zc_ctx *ctx;
+static struct zc_ctx *ctx;
+static const char *file_ptext = NULL;
+static const char *file_cipher = NULL;
+static size_t plain_begin = 0;
+static size_t plain_end = 0;
+static size_t cipher_begin = 0;
+static size_t cipher_end = 0;
+static size_t cipher_remain = 0;
 
 static void print_help(const char *cmdname)
 {
    fprintf(stderr,
            "Usage:\n"
-           "\t%s [options] filename\n"
+           "\t%s plain:begin:end cipher:begin:end:upto \n"
+           "\n"
+           "Example:\n"
+           "cleartext.bin contains 250 cleartext bytes from offset 100 to\n"
+           "350. These are mapped to archive.zip from 340 to 590. The encrypted\n"
+           "stream in archive.zip is followed up to offset 300.\n"
+           "   yazc plaintext cleartext.bin:100:350 archive.zip:340:590:300\n"
            "Options:\n"
            "\t-h, --help              show this help\n",
            cmdname);
 }
 
+static int parse_offset(const char *tok, size_t *offset)
+{
+   char *endptr;
+   long int val = strtol(tok, &endptr, 0);
+   if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN)) ||
+       (errno != 0 && val == 0))
+      return -1;
+   if (endptr == tok)
+      return -1;
+   if (val < 0)
+      return -1;
+   *offset = val;
+   return 0;
+}
+
+static int parse_opt(char *opt, int count, const char **filename, size_t *off1, size_t *off2, size_t *off3)
+{
+   char *saveptr, *token;
+   int err;
+
+   token = strtok_r(opt, ":", &saveptr);
+   if (token == NULL)
+      return -1;
+   *filename = token;
+
+   for (int i = 0; i < count; ++i)
+   {
+      token = strtok_r(NULL, ":", &saveptr);
+      if (token == NULL)
+         return -1;
+      switch (i)
+      {
+      case 0: err = parse_offset(token, off1); break;
+      case 1: err = parse_offset(token, off2); break;
+      case 2: err = parse_offset(token, off3); break;
+      }
+      if (err < 0)
+         return -1;
+   }
+
+   /* TODO: remove */
+   printf("filename: %s\n", *filename);
+   printf("off1: %d\n", off1 != NULL ? *off1 : 0);
+   printf("off2: %d\n", off2 != NULL ? *off2 : 0);
+   printf("off3: %d\n", off3 != NULL ? *off3 : 0);
+
+   return 0;
+}
+
 static int do_plaintext(int argc, char *argv[])
 {
-   const char *filename = NULL;
    int err = 0;
 
    for (;;)
@@ -67,15 +130,34 @@ static int do_plaintext(int argc, char *argv[])
       }
    }
 
-   if (optind >= argc)
+   if (optind + 1 >= argc)
    {
-      fputs("Error: missing filename\n", stderr);
+      fputs("Error: incorrect arguments\n", stderr);
+      print_help(basename(argv[0]));
       return EXIT_FAILURE;
    }
 
-   filename = argv[optind];
-   printf("Filename: %s\n", filename);
-   
+   /* parse plaintext source */
+   /* TODO: remove */
+   printf("Plaintext: %s\n", argv[optind]);
+   printf("Cipher: %s\n", argv[optind + 1]);
+
+   err = parse_opt(argv[optind], 2, &file_ptext,
+                   &plain_begin, &plain_end, NULL);
+   if (err < 0)
+   {
+      fprintf(stderr, "Error parsing plaintext file offsets.\n");
+      return EXIT_FAILURE;
+   }
+
+   err = parse_opt(argv[optind + 1], 3, &file_cipher,
+                   &cipher_begin, &cipher_end, &cipher_remain);
+   if (err < 0)
+   {
+      fprintf(stderr, "Error parsing cipher file offsets.\n");
+      return EXIT_FAILURE;
+   }
+
    zc_new(&ctx);
    if (ctx == NULL)
    {
