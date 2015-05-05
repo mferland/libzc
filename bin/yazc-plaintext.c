045036abdb372b648eea706804ee69e5bbb369aa
Marc Ferland <marc.ferland@gmail.com>
2015-05-05 14:03:48 -0400

    libzc: fix compiler warning

--- bin/yazc-plaintext.c
+++ bin/yazc-plaintext.c
@@ -95,7 +95,7 @@ static int parse_offset(const char *tok, off_t *offset)
 static int parse_opt(char *opt, int count, const char **filename, off_t *off1, off_t *off2, off_t *off3)
 {
     char *saveptr, *token;
-    int err;
+    int err = -1;
 
     token = strtok_r(opt, ":", &saveptr);
     if (!token)
