09e06c98ab45085bb7d181dd378dd1c63babaf5c
Marc Ferland <marc.ferland@gmail.com>
2018-04-01 21:32:34 -0400

    libzc: cosmetic

--- lib/zc_crk_bforce.c
+++ lib/zc_crk_bforce.c
@@ -245,12 +245,12 @@ static int try_decrypt2(const struct zc_crk_bforce *crk, struct worker *w)
 	struct zc_key key;
 	struct hash *h = &w->h;
 
-#define RESET() do \
-    { \
-        key.key0 = h->initk0[ctz]; \
-        key.key1 = h->initk1[ctz]; \
-        key.key2 = h->initk2[ctz]; \
-    } while (0)
+#define RESET() do				\
+	{					\
+		key.key0 = h->initk0[ctz];	\
+		key.key1 = h->initk1[ctz];	\
+		key.key2 = h->initk2[ctz];	\
+	} while (0)
 
 	do {
 		int ctz = __builtin_ctzll(h->candidate);
