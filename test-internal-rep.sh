f3fee2197ced35acfbbc6e86f0a65a74daeec057
Marc Ferland <marc.ferland@gmail.com>
2022-04-27 21:41:33 -0400

    Add test-internal-rep.sh script

---
+++ test-internal-rep.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# 0
+yazc/yazc plaintext -S -i 0x12345678 0x23456789 0x34567890
+
+# 1
+yazc/yazc plaintext -S -i 0x64799c96 0xb303049c 0xa253270a
+
+# 2
+yazc/yazc plaintext -S -i 0x23bd1e23 0x2b7993bc 0x4ccb4379
+
+# 3
+yazc/yazc plaintext -S -i 0x98f19da2 0x1cd05dd7 0x3d945e94
+
+# 4
+yazc/yazc plaintext -S -i 0x2f56297 0x64329027 0xbd806642
+
+# 5
+yazc/yazc plaintext -S -i 0x54dca24b 0x1b079a3b 0x120a6936
+
+# 6
+yazc/yazc plaintext -S -i 0xdbef1574 0xc060416c 0x54cc5d40
+
+# 7
+yazc/yazc plaintext -S -i 0x6d060bfe 0xc76ff413 0x7388dade
+
+# 8
+yazc/yazc plaintext -S -i 0x60dd88de 0xcf040cb6 0x6ac3a828
+
+# 9
+yazc/yazc plaintext -S -i 0x5bbe7395 0xe446ee78 0x92b84d33
+
+# 10
+yazc/yazc plaintext -S -i 0xba8b8876 0xf00562a7 0x02ff2b47
+
+# 11
+yazc/yazc plaintext -S -i 0x83690e4f 0x3ed1c6cf 0x29db36b3
+
+exit 0
