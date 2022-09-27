259c40ed4a491f6b9931366b321293e3d0fbf6fb
Marc Ferland <marc.ferland@gmail.com>
2022-09-27 10:31:26 -0400

    Improve runperf
    Update runperf debug package install instrcutions based on:
    
    https://wiki.ubuntu.com/Debug%20Symbol%20Packages

--- runperf.sh
+++ runperf.sh
@@ -60,13 +60,13 @@ if [ ! -f ${VMLINUX} ]; then
     read -p "Install vmlinux image? (y/n) " yn
     case $yn in
 	[yY] ) echo "Installing vmlinux image..."
-	       echo "deb http://ddebs.ubuntu.com $(lsb_release -cs)-updates main restricted universe multiverse
-	       deb http://ddebs.ubuntu.com $(lsb_release -cs)-security main restricted universe multiverse
-	       deb http://ddebs.ubuntu.com $(lsb_release -cs)-proposed main restricted universe multiverse" | \
-		   sudo tee -a /etc/apt/sources.list.d/ddebs.list
+               echo "deb http://ddebs.ubuntu.com $(lsb_release -cs) main restricted universe multiverse
+               deb http://ddebs.ubuntu.com $(lsb_release -cs)-updates main restricted universe multiverse
+               deb http://ddebs.ubuntu.com $(lsb_release -cs)-proposed main restricted universe multiverse" | \
+                   sudo tee -a /etc/apt/sources.list.d/ddebs.list
 	       
 	       sudo apt install ubuntu-dbgsym-keyring
-
+               sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F2EDC64DC5AEE1F6B9C621F0C8CAB6595FDFF622
 	       sudo apt update
 	       sudo apt install linux-image-$(uname -r)-dbgsym
 	       ;;
