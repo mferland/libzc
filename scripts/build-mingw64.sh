6036ddd603343855e7ccbed8bea0ea7b6d4bf3ea
Marc Ferland <marc.ferland@gmail.com>
2023-04-21 16:14:33 -0400

    mingw64

--- scripts/build-mingw64.sh
+++ scripts/build-mingw64.sh
@@ -4,7 +4,8 @@ set -e
 
 S=${1}
 D=${S}/build-mingw64
-MINGW_BIN_PATH=/usr/x86_64-w64-mingw32/lib
+MINGW_LIB_PATH=/usr/x86_64-w64-mingw32/lib
+MINGW_LOCAL_PATH=/usr/x86_64-w64-mingw32/local
 
 source ${S}/scripts/common.sh
 
@@ -12,11 +13,13 @@ prepare_src ${S} ${D}
 
 ./autogen.sh
 cd ${D}
+export PTHREAD_LIBS="-I${MINGW_LOCAL_PATH}/include -L${MINGW_LOCAL_PATH}/lib -lpthreadGC3"
+export PTHREAD_CFLAGS="-I${MINGW_LOCAL_PATH}/include"
 ../configure --host=x86_64-w64-mingw32 \
 	     --enable-static \
 	     --disable-shared \
 	     CFLAGS="-Ofast" \
-	     LDFLAGS="-L/usr/x86_64-w64-mingw32/lib -lmman"
+	     LDFLAGS="-L${MINGW_LIB_PATH} -lmman ${PTHREAD_LIBS}"
 make -j12
 
 V=$(../configure -V | grep 'zc configure' | cut -d' ' -f3)
@@ -24,8 +27,8 @@ ARCHIVE=yazc-v${V}-win64.zip
 ARCHIVE_DIR=yazc-v${V}
 
 mkdir ${D}/${ARCHIVE_DIR}
-cp ${MINGW_BIN_PATH}/libwinpthread-1.dll ${D}/${ARCHIVE_DIR}
-cp ${MINGW_BIN_PATH}/zlib1.dll ${D}/${ARCHIVE_DIR}
+cp ${MINGW_LOCAL_PATH}/bin/pthreadGC3.dll ${D}/${ARCHIVE_DIR}
+cp ${MINGW_LIB_PATH}/zlib1.dll ${D}/${ARCHIVE_DIR}
 cp yazc/yazc.exe ${D}/${ARCHIVE_DIR}
 zip -r ${D}/${ARCHIVE} ./${ARCHIVE_DIR}
 
