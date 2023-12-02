6723435a363c3b218da8e828ce4430e8341639a1
Marc Ferland <marc.ferland@gmail.com>
2023-12-01 20:37:36 -0500

    Install check from distro package feed

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -10,15 +10,15 @@ fi
 
 if [ "$NAME" = "Fedora Linux" ]
 then
-    dnf install -y git make automake libtool autoconf zlib-devel pkg-config texinfo diffutils
+    dnf install -y git make automake libtool autoconf zlib-devel pkg-config texinfo diffutils check
 elif [ "$NAME" = "Ubuntu" ]
 then
     apt update
-    apt install -yq git make automake libtool autoconf zlib1g-dev pkg-config texinfo diffutils
+    apt install -yq git make automake libtool autoconf zlib1g-dev pkg-config texinfo diffutils check
 elif [ "$NAME" = "Debian GNU/Linux" ]
 then
     apt update
-    apt install -yq git make automake libtool autoconf zlib1g-dev pkg-config texinfo diffutils
+    apt install -yq git make automake libtool autoconf zlib1g-dev pkg-config texinfo diffutils check
 else
     echo "ERROR: OS name is not provided."
     exit 1
